dfd3f51fd59618fb97da071b9da9be536afa94d3
Saachi26 <badalsaachi42@gmail.com>
2024-06-08 00:44:27 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./index.css";
 import Board from "./components/Board/Board";
-import React from "react";
-
+import React, { useReducer } from "react";
+import AppContext from "./context/Context";
+import { initGameState } from "./constants";
+import { reducer } from "./reducer/reducer";
 function App() {
+  const [appState, dispatch] = useReducer(reducer, initGameState);
+  const providerState = {
+    appState,
+    dispatch,
+  };
+
   return (
-    <div className="grid h-screen bg-bg_colour">
-      <Board />
-    </div>
+    <AppContext.Provider value={{ providerState }}>
+      <div className="grid h-screen bg-bg_colour">
+        <Board />
+      </div>
+    </AppContext.Provider>
   );
 }
 
